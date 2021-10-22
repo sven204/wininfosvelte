5b22e70ba9f58226d934f0910dca00f687fde5b4
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-10-22 16:48:54 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 let windowslist = [
 		{
-			"name": "Windows 11 22000.194",
-			"version": "21H2",
+			"name": "Windows 11",
+			"build": "21H2",
 			"codename": "Sun Valley",
-			"released": "2021",
+			"rel": "2021",
 			"eol": "N/A"
 		},
 		{
 			"name": "Windows 10",
-			"version": "21H1",
+			"build": "21H1",
 			"codename": "Redstone",
-			"released": "2015",
+			"rel": "2015",
 			"eol": "2025"
 		},
 		{
 			"name": "Windows 7",
-			"version": "NT 6.1",
+			"build": "NT 6.1",
 			"codename": "Blackcomb",
-			"released": "2009",
+			"rel": "2009",
 			"eol": "2020"
 		},
 	]
