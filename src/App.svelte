c4e732939b4c284f0969f2ba3b3ad1456d9e3c05
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-22 16:12:58 +1300

    Format

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,31 @@
 <script>
-	import windowslist from './data';
+  import windowslist from "./data";
 </script>
 
-	<h1>Test!</h1>
-	{#each windowslist as windows}
-		<div class="card">
-		<p>{windows.name} <br> {windows.version} <br> Released: {windows.released}</p>
-		</div>
-		<br>
-	{/each}
-
+<h1>Test!</h1>
+{#each windowslist as windows}
+  <div class="card">
+    <p>
+      {windows.name} <br />
+      {windows.version} <br /> Released: {windows.released}
+    </p>
+  </div>
+  <br />
+{/each}
 
 <style>
-	* {
-		color: white;
-	}
-	.card * {
-		color: black;
-	}
-	.card {
-		height: 100px;
-		background-color: white;
-		border-radius: 5pt;
-		width: 200px;
-		padding: 5px;
-	}
+  * {
+    color: white;
+  }
+  .card * {
+    color: black;
+  }
+  .card {
+    height: 100px;
+    background-color: white;
+    border-radius: 5pt;
+    width: 200px;
+    color: black;
+    padding: 5px;
+  }
 </style>
