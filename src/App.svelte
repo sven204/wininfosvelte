7b260611e519c59758d00f3f23fa1a7626ca46ac
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-10-21 20:16:52 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,61 +1,38 @@
 <script>
-	export let name;
-	let count 0
-	function handleClick() {
-    		count++;
-	}	
-		let windowslist = [
-	{
-		"name": "Windows 11",
-		"version": "Sun Valley",
-		"released": "2021"
-	},
-	{
-		"name": "Windows 10",
-		"version": "Redstone",
-		"released": "2015"
-	},
-	{
-		"name": "Windows 7",
-		"version": "Something IDK",
-		"released": "2009 or something"
-	},
+	// This array has all of the information in the website.
+	let windowslist = [
+		{
+			"name": "Windows 11",
+			"version": "Sun Valley",
+			"released": "2021"
+		},
+		{
+			"name": "Windows 10",
+			"version": "Redstone",
+			"released": "2015"
+		},
+		{
+			"name": "Windows 7",
+			"version": "Something IDK",
+			"released": "2009 or something"
+		},
 	]
 </script>
-
-<main>
-	<h3>WinInfo is back, now in SvelteJS!</h3>
-	<p>Note: This app isn't ready yet, check back later!</p>
-	
-	<button on:click={handleClick}>Clicked {count} {count === 1 ? 'time' : 'times'}</button>
-	<br>
-	<br>
-	{#each windowslist as windows}
-	<div class="card">
+<h1>Test!</h1>
+<!-- This is #each loop in Svelte. It loops over the array of objects, in the variable called "windowslist".-->
+{#each windowslist as windows}
+<div class="card">
 	{windows.name} <br> {windows.version} <br> Released in {windows.released}
-	</div>
-	<br>
-	{/each}
-	</main>
-
+</div>
+<br>
+{/each}
+<!-- PLEASE change these styles. (if you use this help of course) -->
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.card {
+		height: 100px;
+		background-color: orange;
+		width: 200px;
+		color: white;
+		padding: 5px;
 	}
 </style>
