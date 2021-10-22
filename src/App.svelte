188427e06f1361bfb7c3ad496366449400b5cfe1
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-10-21 21:20:19 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,5 @@
 <script>
-	// This array has all of the information in the website.
-	let windowslist = [
-		{
-			"name": "Windows 11 22000.194",
-			"version": "21H2",
-			"codename": "Sun Valley",
-			"released": "2021",
-			"eol": "N/A"
-		},
-		{
-			"name": "Windows 10",
-			"version": "21H1",
-			"codename": "Redstone",
-			"released": "2015",
-			"eol": "2025"
-		},
-		{
-			"name": "Windows 7",
-			"version": "NT 6.1",
-			"codename": "Blackcomb",
-			"released": "2009",
-			"eol": "2020"
-		},
-	]
+	import windowslist from './data.js';
 </script>
 <h1>Test!</h1>
 {#each windowslist as windows}
