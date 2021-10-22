842a9d43d402c085823d616dafaf8bceb9e67d84
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-10-22 17:01:34 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -9,18 +9,29 @@ let windowslist = [
 		{
 			"name": "Windows 10",
 			"build": "21H1",
-			"last": "21H1",
 			"codename": "Redstone",
 			"rel": "2015",
 			"eol": "2025"
 		},
 		{
+			"name": "Windows 8/8.1",
+			"build": "Contribute",
+			"codename": "Blue",
+			"rel": "Contribute",
+			"eol": "2023"
+		},
+		{
 			"name": "Windows 7",
 			"build": "NT 6.1",
-			"last": "N/A",
 			"codename": "Blackcomb",
 			"rel": "2009",
 			"eol": "2020"
 		},
+		{
+			"name": "Windows XP",
+			"build": "NT 5.1",
+			"codname": "Whistler",
+			"eol": "Contribute"
+		}
 	]
 export default windowslist;
